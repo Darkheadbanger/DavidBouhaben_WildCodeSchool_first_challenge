0b74eb04fafc5ab0c56be293cdd8cc723619af02
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-02-19 00:35:04 +0100

    localStorage work and can add name

--- public/js/addName.js
+++ public/js/addName.js
@@ -4,8 +4,9 @@ const memberList = document.querySelector(".member-list"); // list-groupe
 const elements = [];
 
 window.onload = function () {
-  if (JSON.parse(localStorage.getItem("todo-elements")) != null) {
-    [] = JSON.parse(localStorage.getItem("todo-elements"));
+  if (localStorage.getItem("todo-elements") !== null) {
+    let pushLocal = elements.push(localStorage.getItem("todo-elements"));
+    console.log(pushLocal);
     console.log("elements:", elements);
     afficher();
   }
@@ -22,10 +23,10 @@ function ajouterElement(event) {
     elements.push(addText.value.trim());
     // Maintenant, on doit sauvegarder tous cela dans le localstorage pour que la page mémorise les noms ajoutés
     // Si le localstorage est null alors on retourne
-    if (localStorage.getItem("todo-elements") == null) {
-      localStorage.setItem("todo-elements", JSON.stringify(elements));
+    if (localStorage.getItem("todo-elements") === null) {
+      localStorage.setItem("todo-elements", elements);
     } else {
-      localStorage.setItem("todo-elements", JSON.stringify(elements));
+      localStorage.setItem("todo-elements", elements);
     }
     console.log(localStorage.getItem("todo-elements"));
 
@@ -36,25 +37,26 @@ function ajouterElement(event) {
     afficher();
   }
 }
+
 // Une fonction pour affihcer
 function afficher() {
+  // innerString recois un string vide avant de le populer
   memberList.innerHTML = "";
-  for (let i = 0; i < elements.length; i++) {
-    let nameHtmlAdd = `<div class="member-item">${
-      elements[i].charAt(0).toUpperCase() + elements[i].slice(1)
-    }</div>`;
+  // La méthode join créer et renvoie une nouvelle string en concatenant tous les élements d'un tableau
+  // Join peut être fait comme cela aussi join(","): hello,bonjour,Hi
+  // La méthode split avec virgule pour le convertir au tableau
+  // [Hello, bonjour, hi]
+  let newElements = elements.join().split(","); //
+  console.log(newElements);
+  // On parcourt le tableau vide qui été convertit de string au tableau
+
+  for (let i = 0; i < newElements.length; i++) {
+    let nameHtmlAdd = `<div class="member-item">${newElements[
+      i
+    ][0].toUpperCase()}${newElements[i].slice(1)}</div>`;
     memberList.innerHTML += nameHtmlAdd;
+    console.log(newElements[i]);
   }
 }
-
-// function delete(index){
-//     // Ici, on crée une méthode pour effacer, j'utilise la méthode splice sur l'index
-//     // L'index est la position de l'element ou on a définit sur la boucle for
-//     tableauVide.splice(index, 1)
-// }
-
-// function modification(index) {
-
-// }
 
 addButton.addEventListener("click", ajouterElement);
